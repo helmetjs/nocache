bd4fe6f9eaaf3a366ce2aaec46bc77661ae513a0
analog-nico <nicolai.kamenzky@testrails.org>
2015-10-23 01:48:18 +0200

    Added Surrogate-Control header

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function nocache(options) {
   var noEtag = (options || {}).noEtag;
 
   return function nocache(req, res, next) {
+    res.setHeader('Surrogate-Control', 'no-store');
     res.setHeader('Cache-Control', 'private, no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
