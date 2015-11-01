c92767c0b6d592702cb2504693ec6db5dd3f9939
Evan Hahn <me@evanhahn.com>
2015-11-01 07:44:45 -0800

    Remove `private` directive from Cache-Control
    See discussion in https://github.com/helmetjs/nocache/issues/4.
    
    Closes #4.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function nocache(options) {
 
   return function nocache(req, res, next) {
     res.setHeader('Surrogate-Control', 'no-store');
-    res.setHeader('Cache-Control', 'private, no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
     if (noEtag) {
