1c7d2368cb0a48f177f57503e00f8779dfa166ff
Evan Hahn <me@evanhahn.com>
2015-12-18 11:29:33 -0800

    Test that ETags are set by default

--- test/index.js
+++ test/index.js
@@ -7,6 +7,10 @@ var request = require('supertest')
 describe('nocache', function () {
   it('sets headers properly', function (done) {
     var app = connect()
+    app.use(function (req, res, next) {
+      res.setHeader('ETag', 'abc123')
+      next()
+    })
     app.use(nocache())
     app.use(function (req, res) {
       res.end('Hello world!')
@@ -16,6 +20,7 @@ describe('nocache', function () {
       .expect('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
       .expect('Pragma', 'no-cache')
       .expect('Expires', '0')
+      .expect('ETag', 'abc123')
       .end(done)
   })
 
