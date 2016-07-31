a3fb8bb85ea40ce432c3bef483f5a24cdd5a725a
Evan Hahn <me@evanhahn.com>
2016-07-31 09:25:30 -0700

    Remove `noEtag` option

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-var deprecate = require('depd')('nocache')
-
-module.exports = function nocache (options) {
-  options = options || {}
-
-  if (options.hasOwnProperty('noEtag')) {
-    deprecate('noEtag will be removed in the next version of nocache. Please remove this option.')
-  }
-
-  var noEtag = options.noEtag
-
+module.exports = function nocache () {
   return function nocache (req, res, next) {
     res.setHeader('Surrogate-Control', 'no-store')
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
     res.setHeader('Pragma', 'no-cache')
     res.setHeader('Expires', '0')
-    if (noEtag) {
-      res.removeHeader('ETag')
-    }
+
     next()
   }
 }
--- test/index.js
+++ test/index.js
@@ -24,28 +24,6 @@ describe('nocache', function () {
     .end(done)
   })
 
-  it('can be told to squash etags', function (done) {
-    var app = connect()
-    app.use(function (req, res, next) {
-      res.setHeader('ETag', 'abc123')
-      next()
-    })
-    app.use(nocache({ noEtag: true }))
-    app.use(function (req, res) {
-      res.end('Hello world!')
-    })
-
-    request(app).get('/')
-    .expect('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
-    .expect('Pragma', 'no-cache')
-    .expect('Expires', '0')
-    .end(function (err, res) {
-      if (err) { return done(err) }
-      assert.equal(res.header.etag, undefined)
-      done()
-    })
-  })
-
   it('names its function and middleware', function () {
     assert.equal(nocache.name, 'nocache')
     assert.equal(nocache().name, 'nocache')
