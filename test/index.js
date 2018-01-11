6ff06f23c93c4c2af6b2897ed725450d1d68ab24
Evan Hahn <me@evanhahn.com>
2018-01-11 14:31:38 -0600

    Use promises instead of callbacks in test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var connect = require('connect')
 var request = require('supertest')
 
 describe('nocache', function () {
-  it('sets headers properly', function (done) {
+  it('sets headers properly', function () {
     var app = connect()
     app.use(function (req, res, next) {
       res.setHeader('ETag', 'abc123')
@@ -16,14 +16,13 @@ describe('nocache', function () {
       res.end('Hello world!')
     })
 
-    request(app).get('/')
-    .expect('Surrogate-Control', 'no-store')
-    .expect('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
-    .expect('Pragma', 'no-cache')
-    .expect('Expires', '0')
-    .expect('ETag', 'abc123')
-    .expect('Hello world!')
-    .end(done)
+    return request(app).get('/')
+      .expect('Surrogate-Control', 'no-store')
+      .expect('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('Pragma', 'no-cache')
+      .expect('Expires', '0')
+      .expect('ETag', 'abc123')
+      .expect('Hello world!')
   })
 
   it('names its function and middleware', function () {
