e003b5d7938ec841320b9a0fe5372b30518e7f70
Abdullah Danyal <abdullahsaeed92@gmail.com>
2023-06-01 14:56:57 +0300

    Remove Pragma header
     The Pragma header was initially used to provide backward compatibility
     with HTTP/1.0 caches. However, with the widespread adoption of HTTP/1.1
     and the introduction of cache-control directives, the Pragma header has
     become obsolete.
    
     See <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma>
     for details.
    
    See [#26](https://github.com/helmetjs/nocache/pull/26).

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = function nocache() {
       "Cache-Control",
       "no-store, no-cache, must-revalidate, proxy-revalidate"
     );
-    res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", "0");
 
     next();
