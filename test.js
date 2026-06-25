872931f1143e8d19099b90246d1f29d8c6251cce
Evan Hahn <me@evanhahn.com>
2026-06-25 08:58:40 -0500

    Use Node's test runner

--- test.js
+++ test.js
@@ -1,33 +1,35 @@
 const assert = require("node:assert/strict");
+const test = require("node:test");
 
 const nocache = require(".");
 
-const fakeRequest = {};
+test("sets expected headers and calls `next`", (_t, done) => {
+  const fakeRequest = {};
 
-const fakeResponse = {
-  headers: new Map(),
-  setHeader(name, value) {
-    this.headers.set(name, value);
-  },
-};
+  const fakeResponse = {
+    headers: new Map(),
+    setHeader(name, value) {
+      this.headers.set(name, value);
+    },
+  };
 
-let wasNextCalled = false;
-const fakeNext = (...args) => {
-  wasNextCalled = true;
-  assert.strictEqual(args.length, 0);
-};
+  nocache()(fakeRequest, fakeResponse, (...nextArgs) => {
+    assert.strictEqual(nextArgs.length, 0);
 
-nocache()(fakeRequest, fakeResponse, fakeNext);
+    assert.deepStrictEqual(
+      fakeResponse.headers,
+      new Map([
+        ["Surrogate-Control", "no-store"],
+        ["Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate"],
+        ["Expires", "0"],
+      ]),
+    );
 
-assert.deepStrictEqual(
-  fakeResponse.headers,
-  new Map([
-    ["Surrogate-Control", "no-store"],
-    ["Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate"],
-    ["Expires", "0"],
-  ]),
-);
-assert(wasNextCalled);
+    done();
+  });
+});
 
-assert.strictEqual(nocache.name, "nocache");
-assert.strictEqual(nocache().name, "nocache");
+test("names the function and middleware", () => {
+  assert.strictEqual(nocache.name, "nocache");
+  assert.strictEqual(nocache().name, "nocache");
+});
